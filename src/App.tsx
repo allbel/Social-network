6c58617b28b9fe1a6a2838aeadae738c8d197c53
Aleksandr Belozerov <allbel900@gmail.com>
2022-06-28 01:30:51 +0300

    Added browser-router

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,29 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from 'react-router-dom';
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from './components/Settings/Settings';
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <Header />
-            <Navbar />
+        <BrowserRouter>
+            <div className='app-wrapper'>
 
-            <div className='app-wrapper-content'>
-                <Dialogs />
-                {/*<Profile />*/}
+                <Header/>
+                <Navbar/>
+
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
+
             </div>
-
-        </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,19 +5,19 @@ function Navbar() {
     return (
         <nav className={css.nav}>
             <div className={`${css.item} ${css.active}`}>
-                <a href="">Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             <div className={css.item}>
-                <a href="">Messages</a>
+                <a href="/dialogs">Messages</a>
             </div>
             <div className={css.item}>
-                <a href="">News</a>
+                <a href="/news">News</a>
             </div>
             <div className={css.item}>
-                <a href="">Music</a>
+                <a href="/music">Music</a>
             </div>
             <div className={css.item}>
-                <a href="">Settings</a>
+                <a href="/settings">Settings</a>
             </div>
         </nav>
     );
