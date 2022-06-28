15fa4fa0b15d1e4506c7f1c97faed032702200d0
Aleksandr Belozerov <allbel900@gmail.com>
2022-06-28 14:01:29 +0300

    Added routing

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import css from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 function Navbar() {
     return (
         <nav className={css.nav}>
-            <div className={`${css.item} ${css.active}`}>
-                <a href="/profile">Profile</a>
+            <div className={css.item}>
+                <NavLink to="/profile" activeClassName={css.active}>Profile</NavLink>
             </div>
             <div className={css.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs" activeClassName={css.active}>Messages</NavLink>
             </div>
             <div className={css.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news" activeClassName={css.active}>News</NavLink>
             </div>
             <div className={css.item}>
-                <a href="/music">Music</a>
+                <NavLink to="/music" activeClassName={css.active}>Music</NavLink>
             </div>
             <div className={css.item}>
-                <a href="/settings">Settings</a>
+                <NavLink to="/settings" activeClassName={css.active}>Settings</NavLink>
             </div>
         </nav>
     );
