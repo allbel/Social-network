9ee3bb16a703e70d9d8772361673c36eac0c2549
Aleksandr Belozerov <allbel900@gmail.com>
2022-06-28 17:00:10 +0300

    Dialogue page layout

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,19 @@ import css from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={css.dialogs}>
+            <div className={css.dialogsItems}>
+                <div className={css.dialog + ' ' + css.active}>Dimych</div>
+                <div className={css.dialog}>Sveta</div>
+                <div className={css.dialog}>Sasha</div>
+                <div className={css.dialog}>Viktor</div>
+                <div className={css.dialog}>Valera</div>
+            </div>
+            <div className={css.messages}>
+                <div className={css.message}>Hi</div>
+                <div className={css.message}>How are you</div>
+                <div className={css.message}>Yo</div>
+            </div>
         </div>
     );
 };
