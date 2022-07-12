f2a31fe3229ae7f1914b8ec328d3aa27ceddaff1
Aleksandr Belozerov <allbel900@gmail.com>
2022-07-12 18:23:59 +0300

    Ref React

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -16,15 +16,22 @@ function MyPosts(props: MyPostsPropsType) {
             message={p.message}
             likeCounts={p.likeCounts}/>)
 
+    let newPostElement = React.createRef<HTMLTextAreaElement>()
+
+    const addPost = () => {
+        let text = newPostElement.current?.value
+        alert(text)
+    }
+
     return (
         <div className={css.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={ addPost }>Add post</button>
                 </div>
             </div>
             <div className={css.posts}>
