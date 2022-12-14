81eecf45f6c2a5f2136583d197041a5922b6234e
Aleksandr Belozerov <allbel900@gmail.com>
2022-12-15 01:21:53 +0300

    refactor: functional component ProfileStatus hook useEffect (85)

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 type ProfileStatusPropsType = {
     status: string
@@ -9,6 +9,10 @@ const ProfileStatusWithHooks = (props: ProfileStatusPropsType) => {
 
     const [mode, setMode] = useState<boolean>(false)
     const [status, setStatus] = useState<string>(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
 
     const activateEditMode = () => setMode(true)
 
