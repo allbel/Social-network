4f70a19934f2561b4954ca15ff3a311899026713
Aleksandr Belozerov <allbel900@gmail.com>
2023-01-07 02:32:17 +0300

    refactor: auth reducer (90)

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -22,7 +22,7 @@ const initialState: AuthType = {
     isAuth: false
 }
 
-const SET_USER_DATA = 'SET_USER_DATA'
+const SET_USER_DATA = 'auth/SET_USER_DATA'
 
 const authReducer = (state = initialState, action: ActionType): AuthType => {
     switch (action.type) {
@@ -41,35 +41,29 @@ export const setAuthUserData = (id: IdType, email: EmailType, login: LoginType, 
     } as const)
 
 // Thunks
-export const getAuthUserData = () => (dispatch: Dispatch) => {
-    return authAPI.me()
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                const {id, login, email} = response.data.data
-                dispatch(setAuthUserData(id, email, login, true))
-            }
-        })
+export const getAuthUserData = () => async (dispatch: Dispatch) => {
+    const response = await authAPI.me()
+    if (response.data.resultCode === 0) {
+        const {id, login, email} = response.data.data
+        dispatch(setAuthUserData(id, email, login, true))
+    }
 }
 
-export const login = (email: EmailType, password: string, rememberMe: boolean): AuthThunkType => dispatch => {
-    authAPI.login(email, password, rememberMe)
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                dispatch(getAuthUserData())
-            } else {
-                let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error"
-                dispatch(stopSubmit('login', {_error: message}))
-            }
-        })
+export const login = (email: EmailType, password: string, rememberMe: boolean): AuthThunkType => async dispatch => {
+    const response = await  authAPI.login(email, password, rememberMe)
+    if (response.data.resultCode === 0) {
+        dispatch(getAuthUserData())
+    } else {
+        let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error"
+        dispatch(stopSubmit('login', {_error: message}))
+    }
 }
 
-export const logout = () => (dispatch: Dispatch) => {
-    authAPI.logout()
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                dispatch(setAuthUserData(null, null, null, false))
-            }
-        })
+export const logout = (): AuthThunkType => async dispatch => {
+    const response = await authAPI.logout()
+    if (response.data.resultCode === 0) {
+        dispatch(setAuthUserData(null, null, null, false))
+    }
 }
 
 
