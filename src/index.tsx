68e97a3fc7c7a496fe50b631b637ec8b6de04151
Aleksandr Belozerov <allbel900@gmail.com>
2023-01-20 23:10:44 +0300

    public url

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from "./redux/redux-store";
-import {BrowserRouter, HashRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {Provider} from "react-redux";
 
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
